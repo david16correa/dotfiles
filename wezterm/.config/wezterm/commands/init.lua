5f949b9e5a98504fa7fe94b930313c3b0744f296
David Correa <david.correa.bsc@gmail.com>
2025-05-11 02:29:13 -0600

    more dotfiles

---
+++ wezterm/.config/wezterm/commands/init.lua
@@ -0,0 +1,9 @@
+local toggle_transparency = require("commands.toggle-transparency")
+local toggle_tabBar = require("commands.toggle-tabBar")
+
+local M = {
+	toggle_transparency,
+	toggle_tabBar,
+}
+
+return M
