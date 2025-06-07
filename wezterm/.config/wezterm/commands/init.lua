290a1bfc72c597aa232b5dce79095cf787b03c90
David Correa <david.correa.bsc@gmail.com>
2025-06-07 09:34:15 -0600

    wezterm is not needed on a server

--- wezterm/.config/wezterm/commands/init.lua
+++
@@ -1,9 +0,0 @@
-local toggle_transparency = require("commands.toggle-transparency")
-local toggle_tabBar = require("commands.toggle-tabBar")
-
-local M = {
-	toggle_transparency,
-	toggle_tabBar,
-}
-
-return M
--- wezterm/.config/wezterm/constants.lua
+++
@@ -1,6 +0,0 @@
-local M = {}
-M.bg_image = os.getenv("HOME") .. "/.backgrounds/wezTerm.jpg"
-M.bg_image_darker = os.getenv("HOME") .. "/.backgrounds/wezTerm.jpg"
-M.bg_image_darker_blurred = os.getenv("HOME") .. "/.backgrounds/wezTerm.jpg"
-
-return M
