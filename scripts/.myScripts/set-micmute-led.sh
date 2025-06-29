5b589bb3aecae437089f3e08423a995a42f408ba
David Correa <david.correa.bsc@gmail.com>
2025-06-29 16:21:29 -0600

    scripts not needed on a server

--- scripts/.myScripts/set-micmute-led.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# make sure to add the following to visudo:
-# %wheel ALL=(ALL) NOPASSWD: /home/david/.myScripts/set-micmute-led.sh
-# using `sudo visudo`
-echo "$1" > /sys/class/leds/platform::micmute/brightness
