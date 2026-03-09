a0e5b79ae151e6da107583870cb0cd1d9ed2af5b
David Correa <david.correa.msc@gmail.com>
2026-03-08 22:48:24 -0600

    nixos-rebuild: 2026.03.08 22:48:24

--- nix/stable.nix
+++ nix/stable.nix
@@ -12,7 +12,7 @@
   };
   services.displayManager.sddm = {
       enable = true;
-      # wayland.enable = true;
+      wayland.enable = true;
       package = pkgs.kdePackages.sddm;
       theme = "catppuccin-mocha-mauve";
   };
