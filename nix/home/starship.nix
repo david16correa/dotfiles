3be3025bb001830b343ecaf28cfcbabf978ec332
David Correa <david.correa.msc@gmail.com>
2026-03-16 21:10:49 -0600

    nixos-generation 186 - 2026.03.16 21:10:49

--- nix/home/starship.nix
+++ nix/home/starship.nix
@@ -1,57 +1,57 @@
 { inputs, pkgs, unstable, ... }:
 {
   programs.starship = {
-  enable = true;
-  enableZshIntegration = true;
-  enableBashIntegration = true;
-  settings = {
-    format = "$nix_shell$python$os$hostname$directory$git_branch$git_stash$character";
-
-    nix_shell = {
-      format = "(󱄅 $state) ";
-      style = "bright-blue";
-    };
-
-    python = {
-      format = "(\${virtualenv}) ";
-      style = "bright-yellow";
-      detect_env_vars = [];
-    };
-
-    os = {
-      format = "$symbol ";
-      style = "bright-purple";
-      disabled = false;
-    };
-
-    hostname = {
-      format = "$hostname ";
-      style = "bright-purple";
-      ssh_only = false;
-    };
-
-    directory = {
-      format = "$path ";
-      style = "bright-green";
-      truncation_length = 3;
-      truncate_to_repo = false;
-    };
-
-    git_branch = {
-      format = " $branch ";
-      style = "#d2573f";
-      symbol = " ";
-    };
-
-    git_stash = {
-      format = " 󰛉 $count ";
-      style = "#d2573f";
-    };
-
-    character = {
-      success_symbol = "[❯](bright-yellow)";
-      error_symbol = "[❯](red)";
-    };
-  };
+    enable = true;
+    enableZshIntegration = true;
+    enableBashIntegration = true;
+    # settings = {
+    #   format = "$nix_shell$python$os$hostname$directory$git_branch$git_stash$character";
+    #
+    #   nix_shell = {
+    #     format = "(󱄅 $state) ";
+    #     style = "bright-blue";
+    #   };
+    #
+    #   python = {
+    #     format = "(\${virtualenv}) ";
+    #     style = "bright-yellow";
+    #     detect_env_vars = [];
+    #   };
+    #
+    #   os = {
+    #     format = "$symbol ";
+    #     style = "bright-purple";
+    #     disabled = false;
+    #   };
+    #
+    #   hostname = {
+    #     format = "$hostname ";
+    #     style = "bright-purple";
+    #     ssh_only = false;
+    #   };
+    #
+    #   directory = {
+    #     format = "$path ";
+    #     style = "bright-green";
+    #     truncation_length = 3;
+    #     truncate_to_repo = false;
+    #   };
+    #
+    #   git_branch = {
+    #     format = " $branch ";
+    #     style = "#d2573f";
+    #     symbol = " ";
+    #   };
+    #
+    #   git_stash = {
+    #     format = " 󰛉 $count ";
+    #     style = "#d2573f";
+    #   };
+    #
+    #   character = {
+    #     success_symbol = "[❯](bright-yellow)";
+    #     error_symbol = "[❯](red)";
+    #   };
+    # };
 };
 }
