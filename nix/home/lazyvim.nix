0b6c5eb97f5f769eda58955cc68aded5ddb45d23
David Correa <david.correa.msc@gmail.com>
2026-04-01 10:40:25 -0600

    nixos-generation 246 - 2026.04.01 10:40:25

--- nix/home/lazyvim.nix
+++ nix/home/lazyvim.nix
@@ -79,6 +79,25 @@
         }
       '';
 
+      vimtex = ''
+        return {
+          -- basic setup for latex
+          "lervag/vimtex",
+          ft = "tex",
+          lazy = false, -- we don't want to lazy load VimTeX
+          -- tag = "v2.15", -- uncomment to pin to a specific release
+          init = function()
+            -- VimTeX configuration goes here, e.g.
+            -- vim.g.vimtex_view_method = "zathura"
+            vim.g.vimtex_view_general_viewer = "evince"
+            vim.g.vimtex_compiler_method = "latexmk"
+
+            vim.keymap.set("n", "<leader>ll", ":VimtexCompile<CR>", { desc = "vimtex-compile" })
+            vim.keymap.set("n", "<leader>lc", ":VimtexCompile<CR>", { desc = "vimtex-clean-aux" })
+          end,
+        },
+      '';
+
       vim-slime = ''
         return {
           "jpalardy/vim-slime",
