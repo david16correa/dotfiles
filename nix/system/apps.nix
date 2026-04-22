cd8b4d6c48f52556c3658fd1e0335259e530ef12
David Correa <david.correa.msc@gmail.com>
2026-04-21 23:29:41 -0600

    nixos-generation 278 - 2026.04.21 23:29:41

--- nix/system/apps.nix
+++ nix/system/apps.nix
@@ -31,6 +31,12 @@
     pavucontrol
     easyeffects
     libreoffice-fresh
+    gst_all_1.gstreamer
+    gst_all_1.gst-plugins-base
+    gst_all_1.gst-plugins-good
+    gst_all_1.gst-plugins-bad
+    gst_all_1.gst-plugins-ugly
+    gst_all_1.gst-libav
     gimp
     (symlinkJoin {
       name = "inkscape-with-textext-fixed";
