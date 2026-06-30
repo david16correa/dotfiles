695ce1efbd877104ea74f49a10fc1f1ed464f9eb
David Correa <david.correa.msc@gmail.com>
2026-06-30 14:17:49 -0600

    fix(home-manager): home-manager is building dolphin-emu; switching to flatpak

--- home/david/software/apps.nix
+++ home/david/software/apps.nix
@@ -24,6 +24,7 @@
     apps = [
       "us.zoom.Zoom"
       "com.discordapp.Discord"
+      "org.DolphinEmu.dolphin-emu"
     ];
   };
 
@@ -61,7 +62,6 @@
     showtime # video player
     pavucontrol
     qbittorrent
-    dolphin-emu
     inputs.zen-browser.packages."${pkgs.stdenv.hostPlatform.system}".default
 
     # inkscape stuff
