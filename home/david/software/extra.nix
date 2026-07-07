9cc5ac9f4b64b7c7d652d1ba232d3c30eea2ac47
David Correa <david.correa.msc@gmail.com>
2026-07-06 21:54:40 -0600

    tweak(session variables)

--- home/david/software/extra.nix
+++ home/david/software/extra.nix
@@ -33,4 +33,13 @@
     wl-clipboard
   ];
 
+  ########################################
+  # session variables
+  ########################################
+
+  systemd.user.sessionVariables = {
+    QT_QPA_PLATFORMTHEME = "gtk3"; # https://docs.noctalia.dev/v4/getting-started/faq/#why-are-some-of-my-app-icons-missing
+    STEAM_EXTRA_COMPAT_TOOLS_PATHS = "${config.home.homeDirectory}/.steam/root/compatibilitytools.d";
+  };
+
 }
--- home/david/software/unstable.nix
+++ home/david/software/unstable.nix
@@ -10,12 +10,4 @@
     noctalia-shell
   ];
 
-  ########################################
-  # extra session variables
-  ########################################
-
-  systemd.user.sessionVariables = {
-    QT_QPA_PLATFORMTHEME = "gtk3"; # https://docs.noctalia.dev/v4/getting-started/faq/#why-are-some-of-my-app-icons-missing
-  };
-
 }
